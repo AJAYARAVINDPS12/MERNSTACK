8107d36bd38dbc033c59a903fafb048c0005e537
AJAYARAVINDPS12 <aravindajaysp@gmail.com>
2025-01-20 18:07:22 +0530

    day7(fn)

---
+++ demoproject/src/FunctionComponent/Gallery.jsx
@@ -0,0 +1,9 @@
+const Gallery = () => {
+    return(
+        <div>
+            <h2>This is about Gallery</h2>
+            <img src="SECE.jpg" alt="SECE LOGO"/>
+        </div>
+    )
+}
+export default Gallery;
