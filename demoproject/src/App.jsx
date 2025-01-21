71c050052a0575ab4d87a5fccbc4ea91d835e40c
AJAYARAVINDPS12 <aravindajaysp@gmail.com>
2025-01-21 12:15:18 +0530

    day8(FN)

--- demoproject/src/App.jsx
+++ demoproject/src/App.jsx
@@ -2,16 +2,20 @@ import About from "./FunctionComponent/About";
 import Constact from "./FunctionComponent/Contact";
 import Gallery from "./FunctionComponent/Gallery";
 import Home from "./FunctionComponent/Home";
+import Navbar from "./FunctionComponent/Navbar";
 
 
 function App() {
   return (
+    <>
+    <Navbar />
     <section>
       <About/>
       <Gallery/>
       <Home/>
       <Constact/>
     </section>
+    </>
   );
 }
 
